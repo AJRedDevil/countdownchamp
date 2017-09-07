1bc4a2fe15e5a6db08c4c40a14099768c13d58b0
AJRedDevil <ajan.shresh@gmail.com>
2017-09-07 11:12:53 +0545

    Calculated the time left from deadline using moment

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './App.css';
 
 class Clock extends Component {
@@ -9,7 +10,30 @@ class Clock extends Component {
             hours: 0,
             minutes: 0,
             seconds: 0
-        }
+        };
+        this.getTimeUntil = this.getTimeUntil.bind(this);
+    }
+
+    componentWillMount() {
+        this.getTimeUntil(this.props.deadline);
+    }
+    
+    componentDidMount() {
+        setInterval(() => {
+            this.getTimeUntil(this.props.deadline);
+        }, 1000);
+    } 
+
+    getTimeUntil(deadline) {
+        const time = moment.duration(
+            moment(deadline, "MMMM DD, YYYY").diff(moment())
+        );
+        this.setState({
+            days: parseInt(time.asDays(), 10),
+            hours: time.hours(),
+            minutes: time.minutes(),
+            seconds: time.seconds()
+        });
     }
     
     render() {
