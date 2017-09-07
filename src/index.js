faa9d06261b10bedc7d036ae09f3c2aa3ebd6766
AJRedDevil <ajan.shresh@gmail.com>
2017-09-07 09:40:29 +0545

    First component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 ReactDOM.render(
-    <div>countdown champ</div>,
+    <App />,
     document.getElementById('root')
 );
