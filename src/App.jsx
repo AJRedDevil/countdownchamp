6f2d63cad9676c4fe6871a0abeba28dcba3d1729
AJRedDevil <ajan.shresh@gmail.com>
2017-09-07 10:07:51 +0545

    Update state from user input

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deadline: 'December 27, 2017'
+            deadline: 'December 27, 2017',
+            newDeadline: ''
         };
         this.changeDeadline = this.changeDeadline.bind(this);
     }
 
     changeDeadline() {
         this.setState({
-            deadline: 'December 25, 2017'
+            deadline: this.state.newDeadline
         });
     }
     
@@ -29,7 +30,11 @@ class App extends Component {
                     <div className="Clock-seconds">20 seconds</div>
                 </div>
                 <div>
-                    <input type="text" placeholder="new date"/>
+                    <input
+                        type="text"
+                        placeholder="new date"
+                        onChange={event =>  this.setState({newDeadline: event.target.value})}
+                    />
                     <button onClick={this.changeDeadline}>Submit</button>
                 </div>
             </div>
